96dc955180f992c88dcd06734c293bfa4aa5b02a
1emre <emredmrbs99@gmail.com>
2022-09-28 11:49:17 +0300

    make it better3

--- models/Photo.js
+++ models/Photo.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema;
 
 //create Schema
 const PhotoSchema = new Schema({
-    title: String,
-    description: String,
-    image: String,
-    dateCreated: {
-        type:Date,
-        default: Date.now()
-    }
+  title: String,
+  description: String,
+  image: String,
+  dateCreated: {
+    type: Date,
+    default: new Date.now(),
+  },
 });
 
-const Photo = mongoose.model('Photo',PhotoSchema);
+const Photo = mongoose.model('Photo', PhotoSchema);
 
 module.exports = Photo;
