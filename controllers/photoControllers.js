07181f5e784486be7ee21b983c5a40ec1ab35f91
1emre <emredmrbs99@gmail.com>
2022-09-28 11:22:50 +0300

    Ready For Heroku

---
+++ controllers/photoControllers.js
@@ -0,0 +1,70 @@
+const Photo = require('../models/Photo');
+const fs = require('fs');
+
+exports.getAllPhotos = async (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //   const photos = await Photo.find({}).sort('-dateCreated');
+  //   res.render('index', {
+  //     photos,
+  //   });
+  const page = req.query.page || 1;
+  const photoPerPage = 61;
+
+  const totalPhotos = await Photo.find().countDocuments();
+
+  const photos = await Photo.find({})
+    .sort('-dateCreated')
+    .skip((page - 1) * photoPerPage)
+    .limit(photoPerPage);
+
+  res.render('index', {
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photoPerPage),
+  });
+};
+exports.getPhoto = async (req, res) => {
+  //console.log(req.params.id);
+  //res.render('about');
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+};
+exports.createPhoto = async (req, res) => {
+  //console.log(req.files.image);
+  // await Photo.create(req.body);
+  // res.redirect('/');
+
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    // Bunun için const fs = require('fs'); almamız gerekir.
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/../public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
+};
+exports.updatePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
+};
+exports.deletePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deleteImage = __dirname + '/../public' + photo.image;
+  fs.unlinkSync(deleteImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
+};
---
+++ test.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//connect localDb
+// mongoose.connect('mongodb://localhost/pcat-test-db', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+//connect DB
+mongoose.connect('mongodb+srv://dpedemirbas:admin@mongode.b9b53vk.mongodb.net/pcat-test-db?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
+
+//create Schema
+const PhotoSchema = new Schema({
+    title: String,
+    description: String  
+});
+
+const Photo = mongoose.model('Photo',PhotoSchema);
+
+//crete a photo
+// Photo.create({
+//     title: "Photo Title 1",
+//     description: "Photo description 1 lorem ipsum"
+// });
+
+// Photo.create({
+//     title: "Photo Title 2",
+//     description: "Photo description 2 lorem ipsum"
+// });
+
+//read photo
+// Photo.find({}, (err,data) => {
+//     console.log(data);
+// });
+
+//update photo
+// const id = '6332ae17e85976459ee7db12';
+
+// Photo.findByIdAndUpdate(id,{
+//     title: 'Photo Title 11 updated',
+//     description: 'Photo description 11 updated'
+//     },
+//     {
+//         new: true
+//     },
+//     (err,data) => {
+//         console.log(data);
+//     }
+// );
+
+//delete photo
+const id = '6332b366a69a6c602c4efc10';
+
+Photo.findByIdAndDelete(id,(err,data) => {
+    console.log('Photo is removed...');
+});
